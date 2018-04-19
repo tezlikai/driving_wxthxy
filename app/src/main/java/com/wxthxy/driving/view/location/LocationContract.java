bb7ddb46878235f25db6647508a095f4fd7034ed
likai <sdtezlikai@163.com>
2018-04-19 13:36:20 +0800

    app 添加签名文件

---
+++ app/src/main/java/com/wxthxy/driving/view/location/LocationContract.java
@@ -0,0 +1,19 @@
+package com.wxthxy.driving.view.location;
+
+import com.wxthxy.driving.mvp.BasePresenter;
+import com.wxthxy.driving.mvp.BaseView;
+
+/**
+ * Created by lk on 18-4-17.
+ */
+
+public class LocationContract {
+
+    interface View extends BaseView {
+
+    }
+
+    interface  Presenter extends BasePresenter<View> {
+
+    }
+}
---
+++ app/src/main/java/com/wxthxy/driving/view/location/LocationPresenter.java
@@ -0,0 +1,10 @@
+package com.wxthxy.driving.view.location;
+
+import com.wxthxy.driving.mvp.BasePresenterImpl;
+
+/**
+ * Created by lk on 18-4-17.
+ */
+
+public class LocationPresenter extends BasePresenterImpl<LocationContract.View> implements LocationContract.Presenter{
+}
